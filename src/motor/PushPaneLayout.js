edc073aaa82bb4717acb82d505f1f8d4a1e9552e
joseph pea <jpea@imvu.com>
2017-04-29 12:23:07 -0700

    CONTINUE

--- src/motor/PushPaneLayout.js
+++ src/motor/PushPaneLayout.js
@@ -1,9 +1,17 @@
 import Node from './Node'
+import MotorHTMLPushPaneLayout from '../motor-html/push-pane-layout'
 
 export default
 class PushPaneLayout extends Node {
     constructor(...args) {
         console.log(' -- PushPaneLayout created')
         super(...args)
+    }
+
+    /**
+     * @override
+     */
+    _makeElement() {
+        return new MotorHTMLPushPaneLayout
     }
 }
